96a34eb9ba7679e7c997262aba66ab7e7878efd8
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-11-15 12:29:29 -0300

    solucion final del ejercicio de diagonal de matrices

--- difDiagonal.js
+++ difDiagonal.js
@@ -7,15 +7,19 @@ For example, the square matrix  is shown below:
 4 5 6
 9 8 9  
 The left-to-right diagonal = 1+5+9= 15. The right to left diagonal = 3+5+9= 17 Their absolute difference is |15 - 17| = 2;.
-
+-1 1 -7 -8
+-10 -8 -5 -2
+0 9 7 -1
+4 4 -2 1
 
  */
   let arr = [[1,2,3],[4,5,6],[9,8,9]];
+  let arr2 = [[-1, 1, -7 ,-8],[-10, -8, -5 ,-2],[0 ,9, 7 ,-1],[4, 4 ,-2,1]];
 let primaryDiagonal=0;
 let secondaryDiagonal=0;
 let difference =0;
 
-let absDiagDiff = (arr) => {
+let absDiagDiff = (arr2) => {
     
     for(let i=0 ;i< arr.length; i++){
        
@@ -32,11 +36,11 @@ let absDiagDiff = (arr) => {
  if( difference < 0){
    difference =   difference*-1;
   
-   return difference;
-  }else{
+  }
+  console.log(difference)
     return difference;
- }
+ 
   
 }
 
-absDiagDiff(arr);
+absDiagDiff(arr2);
