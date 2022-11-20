9b92548c5ebf40674a2773cd8bc33c988ce6cf81
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-11-20 17:09:53 -0300

    se resuelve vela de cumple de hacker rank

---
+++ birthdayCandles.js
@@ -0,0 +1,24 @@
+
+
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    // let candles =[3,2,1,3]  las mas altas son dos de 3 unidades de /// altura por lo que el return debe ser un dos
+    let mayor = 0;
+    let biggestCandle =[]; 
+   for(let i = 0; i < candles.length; i++){
+       if( candles[i] > mayor ){
+        mayor=candles[i];
+        console.log(mayor)
+           biggestCandle=[];
+           biggestCandle.push(candles[i]);
+       }else if(candles[i] == mayor){
+        biggestCandle.push(candles[i]);
+       }
+   }
+   console.log(biggestCandle.length)
+   return biggestCandle.length;
+
+}
+let candles =[3,2,1,3]  
+birthdayCakeCandles(candles);
