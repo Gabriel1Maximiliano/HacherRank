91343eacc52d1697ae317fa685e2ffc1b8b70640
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-01-02 19:51:01 -0300

    jumpingClouds resolved

--- jumppingOnTheClouds.js
+++ jumppingOnTheClouds.js
@@ -2,6 +2,17 @@ function jumpingOnClouds(c, k) {
 
     console.log({c})
     console.log({k})
+    let cantidadDeNubes = c.length;
+    let energia = 100;
+    let indiceNube =0;
+    do{
+        if((c[(indiceNube + k)%cantidadDeNubes] == 1)){
+            energia= energia -2;
+        }
+      energia--;
+      indiceNube = (indiceNube + k )%cantidadDeNubes;
+    }while( indiceNube !== 0 )
+    return energia;
     }
 
     {
